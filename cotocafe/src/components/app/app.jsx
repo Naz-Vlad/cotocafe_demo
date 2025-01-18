14393d951f934c8e152e4150f5b5b74063b250ad
Владимир <10nva63@gmail.com>
2025-01-18 10:31:01 +0300

    2

--- cotocafe/src/components/app/app.jsx
+++ cotocafe/src/components/app/app.jsx
@@ -1,9 +1,9 @@
 export default function App() {
 	return (
 	  <div className="App">
-		<h1>КОТОКАФЕ</h1>
-		<h2>Пробую наладить репозиторий</h2>
-		<p>закончил шаблон</p>
-	  </div>
+			<header>ШАПКА</header>
+			<main>ТЕЛО</main>
+			<footer>ПОДВАЛ</footer>
+		</div>
 	);
   }
---
+++ cotocafe/src/components/pages/main-page/main-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainPage() {
+  return (
+	  <div>
+			<h1>КОТОКАФЕ</h1>
+			<h2>11111 Пробую наладить репозиторий</h2>
+			<p>закончил шаблон</p>
+	  </div>
+	)
+}
+
+export default MainPage;
