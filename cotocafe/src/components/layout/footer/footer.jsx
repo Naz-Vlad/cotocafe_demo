7de8ef0599d71cd03a61ab783cdd780c8e19128c
Владимир <10nva63@gmail.com>
2025-01-20 10:11:53 +0300

    сделал подвал

--- cotocafe/src/components/layout/footer/footer.jsx
+++ cotocafe/src/components/layout/footer/footer.jsx
@@ -1,7 +1,13 @@
+import "./style.css"
+import Logo from "/src/components/ui/logo/logo"
 
 function Footer() {
 	return (
-	  <header>ПОДВАЛ</header>
+		<footer className="footer">
+			<Logo/>
+			<span className="footer__copyright">Создано 2021</span>
+		</footer>
 	)
   }
-  export default Footer
+
+	export default Footer
--- cotocafe/src/components/layout/header/header.jsx
+++ cotocafe/src/components/layout/header/header.jsx
@@ -1,12 +1,12 @@
-import Logo from "/src/components/ui/logo/logo";
-import Nav from "/src/components/layout/nav/nav"
+import Logo from "../../ui/logo/logo";
+import Nav from "/src/components/layout/nav/nav";
 import "./style.css"
 
 function Header() {
 	return (
 	  <header className="header">
-        <Logo/>
-		<Nav/>
+      <Logo/>
+		  <Nav/>
 	  </header>
 	)
   }
