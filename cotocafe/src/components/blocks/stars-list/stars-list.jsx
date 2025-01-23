2857d51b0838b196541af10877bd6f65bb90aa63
Владимир <10nva63@gmail.com>
2025-01-23 15:31:53 +0300

    добавил функцию для отрисовки карточек(листы списка

--- cotocafe/src/components/blocks/stars-list/stars-list.jsx
+++ cotocafe/src/components/blocks/stars-list/stars-list.jsx
@@ -3,16 +3,24 @@ import StarCard from "../../ui/star-card/star-card";
 import Title from "../../ui/title/title";
 import "./style.css";
 
-function StarsList({stars}) {
+function StarsList({ stars }) {
 	return (
-    <section className="star-list">
-      <Title>Наши звезды</Title>
-			<ul className="star-list__list">
-				<li className="star-list__item">
-					<StarCard />
-				</li>
-			</ul>
-      <Button minWidth={353}>Купить билет</Button>
+		<section className="star-list">
+			{stars?.length ? ( // проверяем списое еотов на массив
+			  <>
+          <Title>Наши звезды</Title>
+			    <ul className="star-list__list">
+				  {/* Внутри компонента StarList нужно каждый элемент массива stars превратить в компонент StarCard. 
+				  Используем для этого map для перебора м, по id - key={star.id} */}
+				    {stars.map((star) => ( 
+					    <li className="star-list__item" key={star.id}>
+								<StarCard {...star} />
+					    </li>
+				    ))}
+			    </ul>
+					<Button minWidth={353}>Купить билет</Button>
+				</>
+		  ) : null}
 		</section>
 	)
 }
