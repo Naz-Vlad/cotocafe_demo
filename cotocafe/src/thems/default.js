d180b4c55bbfff9ede1aa326d0890aa74d327d79
Владимир <10nva63@gmail.com>
2025-02-09 18:42:01 +0300

    4.8-3_1  создал тему приложения  и записал повторяющиеся стили

---
+++ cotocafe/src/thems/default.js
@@ -0,0 +1,19 @@
+// создадим тему приложения, а затем вынесем в тему повторяющиеся величины
+
+export const defaultTheme = {          // темаПоУмолчанию
+  colorBlackForText: "#333333",        // черныйЦветДляТекста
+  colorWhite: "#ffffff",               //  цветБелый
+  colorGray: "#f6f6f6",                //  цветСерый
+  colorForLightBackground: "#fff6d9",  //  цветДляСветлогоФона
+  colorForDarkBackground: "#ffeead",   //  цветДляТемногоФона
+  colorForNewIcon: "#ffb334",          //  цветДляНовойИконки
+  colorForSoftIcon: "#7fc92e",         //  цветДляМягкогоЗначка
+  colorForButton: "#ff732B",           //  цветДляКнопки
+  colorForButtonHover: "#F35714",      //  цветДляНаведенияКурсораНаКнопку
+  pagePadding: "90px",                 //  страницаОтступ
+  indent: "20px",                      //  выемка
+  fontFamily: '"Inter", "Arial", sans-serif', // тип шрифта
+  fontSizeDefault: "18px",             // размерШрифтаПоУмолчанию
+  footerHeight: "80px",                // подвалШапка
+  pageWidth: "1280px"                  // страницаШирина
+};
